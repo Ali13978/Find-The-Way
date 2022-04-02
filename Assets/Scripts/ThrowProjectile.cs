b59711fbf48285b0d1851cf4ac4d2c91b6b630b9
unknown <aliwebconsultant@gmail.com>
2022-04-02 10:20:17 +0500

    Updated Fire

--- Assets/Scripts/ThrowProjectile.cs
+++ Assets/Scripts/ThrowProjectile.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class ThrowProjectile : MonoBehaviour
 {
     public Vector2 direction;
+    [SerializeField] GameObject PointPrefab;
+    [SerializeField] GameObject[] Points;
+    [SerializeField] int NoOfPoints;
 
     [SerializeField] int ThrowForce;
+    [SerializeField] float force;
     [SerializeField] GameObject Projectile;
     // Start is called before the first frame update
     void Start()
     {
-
+        Points = new GameObject[NoOfPoints];
+        for (int i = 0; i < NoOfPoints; i++)
+        {
+            Points[i] = Instantiate(PointPrefab, transform.position , Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
         Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 ArrowPos = transform.position;
@@ -26,16 +34,26 @@ public class ThrowProjectile : MonoBehaviour
         {
             Shoot();
         }
+        for (int i = 0; i < Points.Length; i++)
+        {
+            Points[i].transform.position = PointPosition( i *0.1f);
+        }
     }
 
     private void Shoot()
     {
         GameObject insProjectile = Instantiate(Projectile,transform.position,transform.rotation);
-        insProjectile.GetComponent<Rigidbody2D>().AddForce(transform.right * ThrowForce);
+        insProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * ThrowForce;
     }
 
     private void FaceMouse()
     {
         transform.right = direction;
+    }
+
+    Vector2 PointPosition(float t)
+    {
+        Vector2 CurrentPointPos = (Vector2)transform.position + (direction.normalized * force * t) + 0.5f*Physics2D.gravity*(t*t);
+        return CurrentPointPos;
     }
 }
