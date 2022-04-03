eeb45282de6b890b3dda7f0e34561bc409900dc1
unknown <aliwebconsultant@gmail.com>
2022-04-03 15:50:29 +0500

    Setting up for android

---
+++ Assets/Scripts/FireManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireManager : MonoBehaviour
+{
+    [SerializeField] float Power = 10f;
+    [SerializeField] float MaxDrag = 5f;
+    
+    Rigidbody2D ProjectileRigidBody;
+    LineRenderer lr;
+    
+    Vector3 DragStartPos;
+    Touch touch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                DragStart();
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Dragging();
+            }
+            if(touch.phase == TouchPhase.Ended)
+            {
+                DragRelease();
+            }
+        }
+    }
+
+    void DragStart()
+    {
+        DragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        DragStartPos.z = 0f;
+        lr.positionCount = 1;
+        lr.SetPosition(0,DragStartPos);
+    }
+
+    void Dragging()
+    {
+        Vector3 Draggingpos = Camera.main.ScreenToWorldPoint(touch.position);
+        Draggingpos.z = 0f;
+        lr.positionCount = 2;
+        lr.SetPosition(1,Draggingpos);
+    }
+
+    void DragRelease()
+    {
+        lr.positionCount = 0;
+        
+    }
+
+}
